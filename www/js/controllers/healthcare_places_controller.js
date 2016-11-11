feec4702f112ab1fc31be50bb30c024ad67baef7
lairjr <lairjr@outlook.com>
2016-11-11 01:24:07 -0200

    Add places service

--- www/js/controllers/healthcare_places_controller.js
+++ www/js/controllers/healthcare_places_controller.js
@@ -1,17 +1,14 @@
 angular.module('starter.controllers')
-.controller('HealthcarePlacesCtrl', function($scope) {
+.controller('HealthcarePlacesCtrl', function($scope, $stateParams, placesService) {
   $scope.init = function () {
-    console.log('Arrived');
+    $scope.loadPlaces();
+  };
+
+  $scope.loadPlaces = function () {
+    var promise = placesService.getByPosition($stateParams.lng, $stateParams.lat);
+
+    promise.then(function (places) {
+      console.log(places);
+    });
   };
 });
-// $scope.findHospitals = function () {
-//   debugger;
-//   var service = new google.maps.places.PlacesService($scope.map);
-//   var pyrmont = new google.maps.LatLng(-30.0573828,-51.1806058);
-//   var request = {
-//     location: pyrmont,
-//     radius: '10000', //10.000 meters
-//     types: ['hospital']
-//   };
-//   service.nearbySearch(request, callback);
-// };
---
+++ www/js/services/places_service.js
@@ -0,0 +1,17 @@
+angular.module('starter.services', []).service('placesService', function($resource, $q) {
+  var placesSvc = $resource('http://saudeemfocoapi.herokuapp.com/places/:lng/:lat');
+
+  return {
+    getByPosition: function(lng, lat) {
+      var deferred = $q.defer();
+
+      placesSvc.query({ lng: lng, lat: lat }).$promise.then(function (data) {
+        deferred.resolve(data);
+      }, function (error) {
+        deferred.reject(error);
+      });
+
+      return deferred.promise;
+    }
+  }
+});
