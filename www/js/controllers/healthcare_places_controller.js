af6dbbf09348ba62292022cafb73e56bc957b229
lairjr <lairjr@outlook.com>
2016-11-11 01:56:40 -0200

    Display hospital list

--- www/js/controllers/healthcare_places_controller.js
+++ www/js/controllers/healthcare_places_controller.js
@@ -4,10 +4,15 @@ angular.module('starter.controllers')
     $scope.loadPlaces();
   };
 
+  $scope.placeList = [];
+
   $scope.loadPlaces = function () {
     var promise = placesService.getByPosition($stateParams.lng, $stateParams.lat);
 
     promise.then(function (places) {
+      angular.forEach(places, function (place) {
+        $scope.placeList.push(place);
+      });
     });
   };
 });
