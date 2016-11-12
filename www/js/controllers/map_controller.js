e8590db50b8a5b74dc1d357449a10347faa664ff
lairjr <lairjr@outlook.com>
2016-11-12 16:22:01 -0200

    Adjust marker icons

--- www/js/controllers/map_controller.js
+++ www/js/controllers/map_controller.js
@@ -72,19 +72,25 @@ angular.module('starter.controllers')
   };
 
   $scope.centerOnMe = function () {
-    console.log("Centering");
     if (!$scope.map) {
       return;
     }
 
     $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
+      content: 'Obtendo posição...',
       showBackdrop: false
     });
 
     navigator.geolocation.getCurrentPosition(function (pos) {
-      console.log('Got pos', pos);
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      var currentPosition = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+
+      var currentMark = new google.maps.Marker({
+        map: $scope.map,
+        position: currentPosition,
+        icon: 'img/tooltip_pulse.gif'
+      });
+
+      $scope.map.setCenter(currentPosition);
       $scope.loading.hide();
     }, function (error) {
       alert('Unable to get location: ' + error.message);
