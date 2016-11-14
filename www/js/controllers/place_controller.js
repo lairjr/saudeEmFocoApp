fd402157dba9dde8ecad216038c508e59c6c1f2d
lairjr <lairjr@outlook.com>
2016-11-14 01:22:07 -0200

    Add display time function

--- www/js/controllers/place_controller.js
+++ www/js/controllers/place_controller.js
@@ -41,6 +41,20 @@ angular.module('starter.controllers')
     });
   };
 
+  $scope.displayTime = function (totalSeconds) {
+    var hours   = Math.floor(totalSeconds / 3600);
+    var minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
+    var seconds = totalSeconds - (hours * 3600) - (minutes * 60);
+
+    // round seconds
+    seconds = Math.round(seconds * 100) / 100
+
+    var result = (hours < 10 ? "0" + hours : hours);
+        result += ":" + (minutes < 10 ? "0" + minutes : minutes);
+        result += ":" + (seconds  < 10 ? "0" + seconds : seconds);
+    return result;
+  };
+
   function findByPlaceId(places, placeId) {
     for (var i = 0; i < places.length; i++) {
       if (places[i].place_id === placeId) {
